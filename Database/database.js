cb3b81e64269cd17a5733b5d8409543ff2bcd4f4
rakibkibria <1905098@ugrad.cse.buet.ac.bd>
2022-08-30 20:01:37 +0600

    bug fixed

--- Database/database.js
+++ Database/database.js
@@ -5,8 +5,8 @@ oracledb.autoCommit = true;
 async function startup() {
     console.log('starting up database.');
     await oracledb.createPool({
-        user: 'c##rifat',
-        password: 'rifat',
+        user: 'c##rakib',
+        password: 'rakib',
         connectstring: 'localhost/orcl',
         poolMin: 4,
         poolMax: 10,
